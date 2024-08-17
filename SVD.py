dde535ddae747adaa076b08e10430f6578bf16f2
Andrew Lytle <atlytle@gmail.com>
2024-08-17 17:22:38 -0500

    Update SVD.ipynb

--- SVD.py
+++ SVD.py
@@ -30,8 +30,6 @@ def to_diag(S, m, n):
 def dotall(Ms):
     "Multiply matrices [M1, M2, ..., Mn] together."
     res = np.identity(Ms[-1].shape[1])
-    print(res)
-    print(res.shape)
     for M in reversed(Ms):
         res = np.dot(M, res)
     return res
@@ -43,20 +41,33 @@ def dotall(Ms):
 
 # %% [markdown]
 # The singular value decomposition is an extremely general matrix factorization, which exists for any $m \times n$ complex matrix $M$.
-# We write $M = U_{m \times m} \Sigma_{m \times n} V^\dagger_{n \times n}$, where $U$ and $V$ are *unitary*, and $\Sigma$ has non-negative real numbers along the diagonal. The size of the matrices has been written out in the subscripts. The elements of $\Sigma$ (called singular values) are uniquely determined and the number of non-zero elements gives the *rank* of $M$. We can take these elements to be in descending order, say, which then makes $\Sigma$ uniquely determined by $M$.
+# We write $M = U_{m \times m} \Sigma_{m \times n} V^\dagger_{n \times n}$, where $U$ and $V$ are *unitary*, and $\Sigma$ has non-negative real numbers along the diagonal. The size of the matrices has been written out in the subscripts. The elements of $\Sigma$ (called singular values) are uniquely determined, and the number of non-zero elements gives the *rank* of $M$. We can take these elements to be in descending order, say, which then makes $\Sigma$ uniquely determined by $M$.
 #
-# Recall that the *rank* of a matrix is the dimension of the vector space spanned by its columns. The number of zero entries give the dimension of the *kernel*, or *null space*, i.e. the vector space that is mapped to 0 by $M$.
+# Recall that the *rank* of a matrix is the dimension of the vector space spanned by its columns. $r \leq \min{(m,n)}$. We can take $\Sigma$ to be $r \times r$ as well (remove zero entries), in which case $M = U_{m \times r} \Sigma_{r \times r} V^\dagger_{r \times n}$ and the columns/rows of $U$/$V^\dagger$ corresponding to zero entries have been removed.
 
 # %%
-M = np.random.randn(9, 6) + 1j*np.random.randn(9, 6)
+m, n = 9, 6  # Matrix dimensions.
+M = np.random.randn(m, n) + 1j*np.random.randn(m, n)
+
+# Full  SVD.
 U, S, Vh = np.linalg.svd(M, full_matrices=True)
-print(U.shape, S.shape, Vh.shape)
-S = to_diag(S, 9, 6)
+print(U.shape, S.shape, Vh.shape)  # S is a list of singular values.
+S = to_diag(S, m, n)  # Convert S to a diagonal matrix.
+np.allclose(M, dotall([U, S, Vh]))
+
+# Reduced SVD.
+U, S, Vh = np.linalg.svd(M, full_matrices=False)
+print(U.shape, S.shape, Vh.shape)  # S is a list of singular values.
+r = S.shape[0]
+S = to_diag(S, r, r)  # Convert S to a diagonal matrix.
 np.allclose(M, dotall([U, S, Vh]))
 
 
 # %% [markdown]
 # # Schmidt decomposition and entanglement
+
+# %% [markdown]
+# The Schmidt decomposition of a vector is essentially a rephrasing of SVD. It can tell us e.g. about entanglement in a quantum state.
 
 # %% [markdown]
 #
